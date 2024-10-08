4889d6001635919a37ded66cba3dbd39d353e9ba
momadAB <mabaqer99@gmail.com>
2024-10-08 13:29:14 +0300

    Extra done

--- iterationMethods.js
+++ iterationMethods.js
@@ -37,6 +37,67 @@ Combine the filter and map methods to perform the following tasks:
 6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
 ******************************************************************/
 
-const numbersGreaterThan20Squared = numbers.filter((number) => (number > 20)).map((number) => number ** 2);
+const numbersGreaterThanOrEqualTo20Squared = numbers.filter((number) => (number >= 20)).map((number) => number ** 2);
+const numbersDivisibleBy5MultipliedBy3 = numbers.filter((number) => (number % 5 === 0)).map((number) => number * 3);
 
-console.log(numbersGreaterThan20Squared);
+/*****************************************************************
+Part 4: Extra not required 🌶️🌶️🌶️
+******************************************************************/
+/*************************************
+ * logger(array)
+ *
+ * - Accepts an array
+ * - Logs every element of the array
+ ************************************/
+
+function logger(array) {
+    array.array.forEach(element => console.log(element));
+}
+
+/*************************************
+ * toCelsius(temperatures)
+ *
+ * - Accepts an array of temperatures
+ *   in degrees Fahrenheit
+ * - Returns an array of temperatures
+ *   in degrees Celsius
+ *
+ * The conversion is:
+ *   C = (F - 32) * (5/9)
+ ************************************/
+
+function toCelsius(temperatures) {
+    return temperatures.map((temperature) => (temperature - 32) * (5/9))
+}
+
+/**************************************
+ * hottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ * - Accepts a threshhold temperature
+ * - Returns an array of temperatures
+ *   that exceed the threshhold
+ ***************************************/
+function hottestDays(temperatures, threshhold) {
+    return temperatures.filter((temperature) => temperature > threshhold);
+}
+
+/******************************************
+ * logHottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ *   IN DEGREES FAHRENHEINT
+ * - Accepts a threshhold temperature
+ *   IN DEGREES FAHRENHEINT
+ * - Logs temperatures that exceed the
+ *   threshhold to the console
+ *   IN DEGREES CELSIUS
+ *
+ * hint: you can combine
+ *       all previous functions
+ *******************************************/
+
+function logHottestDays(temperatures, threshhold) {
+    const hottestDays = toCelsius(hottestDays(temperatures, threshhold));
+    logger(hottestDays);
+}
